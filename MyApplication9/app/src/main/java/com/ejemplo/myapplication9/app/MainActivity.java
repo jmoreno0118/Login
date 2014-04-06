587846283904a05963a8799293c791ce407aa6c9
Jesus Jose Moreno Perez <jesus.jose.moreno@hotmail.com>
2014-04-06 14:52:21 -0600

    Pantallas del login
    Se guarda la "sesion" de usuario

---
+++ MyApplication9/app/src/main/java/com/ejemplo/myapplication9/app/MainActivity.java
@@ -0,0 +1,61 @@
+package com.ejemplo.myapplication9.app;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+
+public class MainActivity extends ActionBarActivity {
+    String NOMBRE = "datos_login";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btn = (Button) findViewById(R.id.btnlogout);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try{
+                    FileOutputStream fileOutputStream = openFileOutput(NOMBRE, Context.MODE_PRIVATE);
+                    fileOutputStream.write("Loggedout".getBytes());
+                    fileOutputStream.close();
+                    Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                    startActivity(intent);
+                    finish();
+                }catch(Exception e){}
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
